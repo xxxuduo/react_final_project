a7f0668d77152074af4ba7f813211c2d6ad1e441
Duo Xu <xxxuduo@berkeley.edu>
2019-05-02 17:20:04 -0700

    first version

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Tabletop from 'tabletop';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1fO0fmOyoXHGfJgvhVLxNlyCdKvkYVS4R8zzEDIBXH9w',
+      callback: googleData => {
+        this.setState({
+          data: googleData
+        })
+      },
+      simpleSheet: true
+    })
+  }
+
+  render() {
+    const { data } = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Team-One React Decal Final Project</h1>
+        </header>
+        <div id="team-details">
+          {
+            data.map(obj => {
+              return (
+                <div key={obj.team_name}>
+                  <p>{obj.team_name}</p>
+                  <p>{obj.members}</p>
+                  <img alt={obj.team_name} src={obj.img} />
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
